9acb36867e7d348d66370d497eefb6f884f2e401
Mica Alaniz <malaniz@leapfrogonline.com>
2017-02-18 16:44:43 -0600

    open sans

--- wp-content/themes/pawar2018/header.php
+++ wp-content/themes/pawar2018/header.php
@@ -5,7 +5,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 <?php wp_head(); ?>
 </head>
