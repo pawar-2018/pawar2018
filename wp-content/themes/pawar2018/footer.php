1cc4d835f811a7b069e496854d27b78bf1ed90fd
Peter <peter.binkowski@gmail.com>
2017-02-18 16:35:33 -0600

    foundation fix

--- wp-content/themes/pawar2018/footer.php
+++ wp-content/themes/pawar2018/footer.php
@@ -1,9 +1,9 @@
 <footer>
 	<div class="row">
 		<div class="large-3 columns">
-			<h1 class="footer-logo">Ameya Pawar </h1>
 			<nav class="footer-nav">
 				<ul>
+					<h1 class="footer-logo">Ameya Pawar </h1>
 					<li><a href="/about">About</a></li>
 					<li><a href="/issues">Issues</a></li>
 					<li><a href="/events">Events</a></li>
