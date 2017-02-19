db917944c983de5b36f0b6702bd1f36080ee6c93
Peter <peter.binkowski@gmail.com>
2017-02-18 18:31:31 -0600

    basic hero section

--- wp-content/themes/pawar2018/page-meet-ameya.php
+++ wp-content/themes/pawar2018/page-meet-ameya.php
@@ -1,7 +1,10 @@
 <?php get_header(); ?>
   <main>
-  	<section class="hero">
-  	</section>
+     <section class="hero" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/meet-ameya.png'); background-size: cover;">
+         <div class="row align-center align-middle">
+             <h1>Meet Ameya</h1>
+        </div>
+    </section>
     <div class="row">
     </div>
   </main>
