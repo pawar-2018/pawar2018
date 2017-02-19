40f424b358d6471508dad952b914da8742a21c6f
Peter <peter.binkowski@gmail.com>
2017-02-18 18:42:11 -0600

    heading font and about main body title

--- wp-content/themes/pawar2018/header.php
+++ wp-content/themes/pawar2018/header.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
 <?php wp_head(); ?>
 </head>
--- wp-content/themes/pawar2018/page-meet-ameya.php
+++ wp-content/themes/pawar2018/page-meet-ameya.php
@@ -1,11 +1,16 @@
 <?php get_header(); ?>
   <main>
-     <section class="hero" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/meet-ameya.png'); background-size: cover;">
+    <section class="hero" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/meet-ameya.png'); background-size: cover;">
          <div class="row align-center align-middle">
-             <h1>Meet Ameya</h1>
+             <h1 class="page-title">Meet Ameya</h1>
         </div>
     </section>
-    <div class="row">
-    </div>
+    <section class="body">
+        <div class="row">
+            <div class="small-12 columns">
+                <h1 class="section-title">Pawar for the people</h1>
+            </div>
+        </div>
+    </section>
   </main>
 <?php get_footer(); ?>
