26d8935f9572b7ba69b591215d0eef08b8b8b48e
Peter <peter.binkowski@gmail.com>
2017-02-18 18:46:40 -0600

    about content

--- wp-content/themes/pawar2018/page-meet-ameya.php
+++ wp-content/themes/pawar2018/page-meet-ameya.php
@@ -6,9 +6,11 @@
         </div>
     </section>
     <section class="body">
-        <div class="row">
-            <div class="small-12 columns">
+        <div class="row align-center">
+            <div class="small-10 columns">
                 <h1 class="section-title">Pawar for the people</h1>
+                <p>The son of Indian immigrants, Alderman Ameya Pawar was born and raised in Illinois. In 2011, he beat the Chicago machine to become alderman of Chicago’s 47th Ward, the first Asian American in the City Council. In 2015, Ameya was re-elected with 82% of the vote, the largest margin in the city.</p>
+                <p>Since taking office, Ameya has secured millions of dollars for schools in his community. Ameya’s work on neighborhood schools—helping families stay in one neighborhood from kindergarten through 12th grade—is seen by many as a model for the city.</p>
             </div>
         </div>
     </section>
