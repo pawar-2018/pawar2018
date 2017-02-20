143d376e5bebc224d6d2c2c37a3e6bedb591dfd2
Mica Alaniz <malaniz@leapfrogonline.com>
2017-02-19 19:51:09 -0600

    yay hero done

--- wp-content/themes/pawar2018/front-page.php
+++ wp-content/themes/pawar2018/front-page.php
@@ -2,10 +2,16 @@
   <main>
   	<section class="hero">
   		<div class="row align-middle">
-  			<div class="small-12 columns">
+  			<div class="small-7 columns">
   				<h1>A New Deal For Illinois</h1>
-  			</div>
-  		</div>
+  				<h2>It‘s time to grab a clipboard, lace up our boots, and work together to restore progressive values in 2018.</h2>
+					<h3>Get the latest updates</h3>
+					<form>
+						<input type="text" placeholder="Email Address"></input>
+						<button type="submit">Subscribe</button>
+					</form>
+				</div>
+			</div>
   	</section>
     <div class="row">
     </div>
