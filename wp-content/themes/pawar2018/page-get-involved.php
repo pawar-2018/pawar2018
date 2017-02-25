0f0d4c898b377747204d1627c57e13982eb715bd
Peter <peter.binkowski@gmail.com>
2017-02-25 13:43:20 -0600

    get involved refactor

--- wp-content/themes/pawar2018/page-get-involved.php
+++ wp-content/themes/pawar2018/page-get-involved.php
@@ -1,37 +1,43 @@
 <?php get_header(); ?>
 <main>
-    <section class="hero">
-  	</section>
-
-    <section class="body">
-      <div class="row">
-        <div class="small-12 medium-8 columns">
-            <h1 class="section-title">We're working to win back Illinois. Join Us.</h1>
-            <br>
-            <br>
-            <br>
-            <script type='text/javascript' src='//d1aqhv4sn5kxtx.cloudfront.net/actiontag/at.js'></script>
-            <div class='ngp-form'
-                data-template='oberon'
-                data-id='3208625543266699776'
-                data-endpoint='https://api.myngp.com/'
-                data-formdef-endpoint='//formdefs.s3.amazonaws.com/api.myngp.com'
-                data-fastaction-endpoint='https://fastaction.ngpvan.com/'
-                data-databag='everybody'
-                data-resource-path='https://d1aqhv4sn5kxtx.cloudfront.net/'
-                data-inline-errors='true'
-                data-fastaction-nologin='true'
-                data-embed='true'>
+<section class="hero">
+</section>
+<section class="wrapper">
+<div class="main body">
+    <div class="row align-center">
+    <div class="small-12 medium-12 columns">
+        <h1 class="section-title">We're working to win back Illinois. Join Us.</h1>
+        <br>
+        <br>
+        <br>
+        <script type='text/javascript' src='//d1aqhv4sn5kxtx.cloudfront.net/actiontag/at.js'></script>
+        <div class='ngp-form'
+            data-template='oberon'
+            data-id='3208625543266699776'
+            data-endpoint='https://api.myngp.com/'
+            data-formdef-endpoint='//formdefs.s3.amazonaws.com/api.myngp.com'
+            data-fastaction-endpoint='https://fastaction.ngpvan.com/'
+            data-databag='everybody'
+            data-resource-path='https://d1aqhv4sn5kxtx.cloudfront.net/'
+            data-inline-errors='true'
+            data-fastaction-nologin='true'
+            data-embed='true'>
+        </div>
+    </div>
+    </div>
+    </div>
+    <div class="sidebar">
+        <div class="row align-center">
+            <div class="small-12 medium-8 columns ">
+                <p>
+                    <strong>AMEYA PAWAR FOR GOVERNOR</strong>
+                    <a href="mailto:info@pawar2018.com">info@pawar2018.com</a><br />
+                    P.O. Box 577162<br />
+                    Chicago, IL 60657<br />
+                </p>
             </div>
         </div>
-        <div class="small-12 medium-4 columns">
-            <p>
-                <strong>AMEYA PAWAR FOR GOVERNOR</strong>
-                info@pawar2018.com<br />
-                P.O. Box 577162<br />
-                Chicago, IL 60657<br />
-            </p>
-        </div>
-    </section>
+    </div>
+</section>
 </main>
 <?php get_footer(); ?>
