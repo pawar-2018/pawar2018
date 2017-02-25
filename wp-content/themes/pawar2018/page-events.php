57dc05b20c93a74d3b2937f9298885cde54d9974
Peter <peter.binkowski@gmail.com>
2017-02-25 12:35:23 -0600

    event content finished

--- wp-content/themes/pawar2018/page-events.php
+++ wp-content/themes/pawar2018/page-events.php
@@ -6,13 +6,53 @@
        </div>
   	</section>
     <section class="body">
-        <div class="row">
-            <div class="small-4 columns">
-                <h1 class="section-title">Let's Meet</h1>
+        <div class="row align-center">
+            <div class="small-12 large-4 columns">
+                <h1 class="section-title">Let's Talk</h1>
+                <br>
+                <br>
+                <br>
+                <p>Looking to host an event?<p>
+                <a href='/get-involved' class="button">Contact Us</a>
             </div>
-            <div class="small-6 columns">
-            </div>
+            <div class="small-12 large-6 columns">
+                <div class="event row">
+                    <div class="large-2 small-4 columns small-centered">
+                    <span class="event-date_circle">
+                        5<br>March
+                    </span>
+                    </div>
+                    <div class="small-11 large-8 columns event-copy">
+                      <p class="event-date">Saturday, March 5th at 11 AM</p>
+                      <h5 class="event-header">The Future of Illinois with Gubernatorial Candidate Ameya Pawar</h5>
+                      <p class="event-locale">Irish American Heritage Center</p>
+                      <p class="event-address">
+                        4626 N Knox Ave
+                        <br />
+                        Chicago IL
+                      </p>
+                  </div>
+              </div>
+              <div class="event row">
+                <div class="large-2 small-4 columns small-centered">
+                    <span class="event-date_circle">
+                        7<br>March
+                    </span>
+                </div>
+                <div class="small-11 large-8 columns event-copy">
+                  <p class="event-date">Friday, March 7th at 9 PM</p>
+                  <h5 class="event-header">The Future of Illinois with Gubernatorial Candidate Ameya Pawar</h5>
+                  <p class="event-locale">Irish American Heritage Center</p>
+                  <p class="event-address">
+                    4626 N Knox Ave
+                    <br />
+                    Chicago IL
+                  </p>
+                </div>
+              </div>
+          </div>
         </div>
-    </section>
-  </main>
+    </div>
+</section>
+</main>
 <?php get_footer(); ?>
