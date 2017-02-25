e41c31806cc49c1478c6524181ee188a93459fdf
Peter <peter.binkowski@gmail.com>
2017-02-25 11:50:48 -0600

    issues page done, and new content

--- wp-content/themes/pawar2018/page-issues.php
+++ wp-content/themes/pawar2018/page-issues.php
@@ -1,12 +1,15 @@
 <?php get_header(); ?>
   <main>
-  	<section class="hero">
+  	<section class="hero" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/issues-hero.jpg'); background-size: cover;">
+        <div class="row align-center align-middle">
+            <h1 class="page-title">Issues</h1>
+       </div>
   	</section>
     <section class="body">
         <div class="row align-center">
             <div class="small-10 columns">
                 <h1 class="section-title">Out with the old, in with the New Deal.</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>President Roosevelt’s New Deal took our country out of depression and laid the foundation for decades of growth and prosperity. We can do the same thing in Illinois by rebuilding our crumbling infrastructure, reinvesting in good-paying jobs, and recommitting to early childhood education and public schools. And we can do it without further burdening the middle class.</p>
             </div>
             <div class="feature-issue large-5 small-12">
                 <img src="<?php echo get_bloginfo('template_url') ?>/assets/school.svg" class="issue-icon">
@@ -98,7 +101,9 @@
             <a href="/events" class="button">See Ameya</a>&nbsp;&nbsp;&nbsp;
             <a href="https://act.myngp.com/Forms/9188561423484586496" target="_blank" class="button">Donate</a>
         </div>
-</div>
+    </div>
 </section>
- </main>
+<section style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/issues-01.jpg'); background-size: cover; height:25.652rem;">
+</section>
+</main>
 <?php get_footer(); ?>
--- wp-content/themes/pawar2018/page-meet-ameya.php
+++ wp-content/themes/pawar2018/page-meet-ameya.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <main>
-    <section class="hero" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/meet-ameya.png'); background-size: cover;">
+    <section class="hero" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/meet-ameya-hero.jpg'); background-size: cover;">
          <div class="row align-center align-middle">
              <h1 class="page-title">Meet Ameya</h1>
         </div>
@@ -11,7 +11,7 @@
                 <h1 class="section-title">Pawar for the people</h1>
                 <p>The son of Indian immigrants, Alderman Ameya Pawar was born and raised in Illinois. In 2011, he beat the Chicago machine to become alderman of Chicago’s 47th Ward, the first Asian American in the City Council. In 2015, Ameya was re-elected with 82% of the vote, the largest margin in the city.</p>
                 <p>Since taking office, Ameya has secured millions of dollars for schools in his community. Ameya’s work on neighborhood schools—helping families stay in one neighborhood from kindergarten through 12th grade—is seen by many as a model for the city.</p>
-                <p><img src="<?php echo get_bloginfo('template_url') ?>/assets/meet-ameya-01.png"></p>
+                <p><img src="<?php echo get_bloginfo('template_url') ?>/assets/meet-ameya-01.jpg"></p>
                 <p>Ameya has led efforts to pass several pieces of progressive legislation focusing on social justice, workers’ rights, and government reforms. Highlights include the Chicago paid sick leave ordinance, the creation of an independent budget office, the passage of one the nation’s strongest anti-wage theft ordinances, and the preservation of affordable housing.</p>
                 <p>Growing up, Ameya attended public schools. He went on to earn two master’s degrees from the University of Chicago and one from the Illinois Institute of Technology. In 2014 he co-authored a textbook on emergency management, disaster policy, social justice, and community resiliency with his wife Charna Epstein. The book proposes a new model for understanding the interconnectedness of communities to help emergency managers better prepare. Ameya and Charna have presented their work at conferences across the country. Charna is now the chief operating officer of the Urban Education Institute at the University of Chicago.</p>
                 <div class="row column">
