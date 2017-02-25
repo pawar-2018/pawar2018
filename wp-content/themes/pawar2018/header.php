2fbe6912cb6bc0309597bd62432f92d4393cf7b3
Mica Alaniz <malaniz@leapfrogonline.com>
2017-02-25 01:18:54 -0600

    favicons are cute

--- wp-content/themes/pawar2018/header.php
+++ wp-content/themes/pawar2018/header.php
@@ -8,6 +8,7 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.0/css/foundation.min.css">
+<link rel="icon" href="<?php echo get_bloginfo('template_url') ?>/assets/favicon.ico" type="image/x-icon" />
 
 <?php wp_head(); ?>
 </head>
