f2680d69b552d0fd1166b7b854a40ef47f8fa0c4
Mica Alaniz <malaniz@leapfrogonline.com>
2017-02-27 14:14:47 -0600

    removing script build since we don't use it but keeping the deps since we probably will

--- gulpfile.js
+++ gulpfile.js
@@ -27,22 +27,21 @@ gulp.task('styles', function() {
       browsers: ['last 2 version'],
       cascade: false
     })]))
-    .pipe(gulp.dest(wpPath))
     .pipe($.cssnano())
     .pipe($.if(!PRODUCTION, $.sourcemaps.write('.')))
     .pipe(gulp.dest(wpPath))
     .pipe($.size({title: 'styles'}));
 });
 
-gulp.task('scripts', function() {
-  return gulp.src(wpPath + 'js/scripts/*.js')
-    .pipe($.if(!PRODUCTION, $.sourcemaps.init()))
-    .pipe($.concat('main.min.js'))
-    .pipe($.uglify({preserveComments: 'some'}))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write('.')))
-    .pipe(gulp.dest(wpPath + 'js'))
-    .pipe($.size({title: 'scripts'}));
-});
+// gulp.task('scripts', function() {
+//   return gulp.src(wpPath + 'js/scripts/*.js')
+//     .pipe($.if(!PRODUCTION, $.sourcemaps.init()))
+//     .pipe($.concat('main.min.js'))
+//     .pipe($.uglify({preserveComments: 'some'}))
+//     .pipe($.if(!PRODUCTION, $.sourcemaps.write('.')))
+//     .pipe(gulp.dest(wpPath + 'js'))
+//     .pipe($.size({title: 'scripts'}));
+// });
 
 gulp.task('serve', function() {
 
@@ -51,8 +50,11 @@ gulp.task('serve', function() {
   });
 
   gulp.watch(wpPath + 'sass/**/*.scss',  gulp.series('styles')).on('change', browserSync.reload);
-  gulp.watch(wpPath + 'js/scripts/*.js', gulp.series('scripts')).on('change', browserSync.reload);
+  // gulp.watch(wpPath + 'js/scripts/*.js', gulp.series('scripts')).on('change', browserSync.reload);
   gulp.watch(wpPath + 'images/*.{svg,png,jpg,gif}',  gulp.series('images')).on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('styles', 'scripts', 'images'));
+gulp.task('default', gulp.series(
+  'styles',
+  // 'scripts',
+  'images'));
