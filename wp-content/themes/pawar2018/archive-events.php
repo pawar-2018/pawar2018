7cbc73a9e0024438bd4e0b097a9c45dd5f3e765a
Peter <peter.binkowski@gmail.com>
2017-03-03 17:15:03 -0600

    events page is totally data driven

--- wp-content/themes/pawar2018/archive-events.php
+++ wp-content/themes/pawar2018/archive-events.php
@@ -13,41 +13,25 @@
                 <a href='/get-involved' class="button">Contact Us</a>
             </div>
             <div class="small-12 medium-5 large-6 small-order-1 medium-order-2 columns">
-                <div class="event row align-center">
-                    <div class="small-3 medium-10 large-2 columns">
-                    <div class="event-date_circle">
-                        4<br>March
-                    </div>
-                    </div>
-                    <div class="small-11 large-7  large-offset-1 columns event-copy">
-                      <p class="event-date">Saturday, March 4th at 1:45PM</p>
-                      <h5 class="event-header">Brady Campaign</h5>
-                      <p class="event-locale">Simeon Career Academy</p>
-                      <p class="event-address">
-                        8147 S Vicennes Ave
-                        <br />
-                        Chicago, IL
-                      </p>
-                  </div>
-              </div>
-              <div class="event row align-center">
-                <div class="small-3 medium-10 large-2 columns">
-                    <div class="event-date_circle">
-                        5<br>March
-                    </div>
-                </div>
-                <div class="small-11 large-7 large-offset-1 columns event-copy">
-                  <p class="event-date">Sunday, March 5th at 4:30PM</p>
-                  <h5 class="event-header">Meet & Greet with Action for a Better Tomorrow - South Suburbs</h5>
-                  <p class="event-locale">Flossmoor Community Church</p>
-                  <p class="event-address">
-                    2218 Hutchinson Rd.
-                    <br />
-                    Flossmoor, IL
-                  </p>
-                </div>
-              </div>
-              <?php $args = array( 'post_type' => 'events', 'posts_per_page' => -1 ); ?>
+            <?php
+              $today = date('Ymd');
+              $args = array(
+              'post_type' => 'events',
+              'posts_per_page' => -1,
+              'meta_key' => 'start_date',
+              'orderby' => 'meta_value_num',
+              'order' => 'ASC',
+              'meta_query'  => array(
+                array(
+                    'key' => 'start_date',
+                    'type' => 'NUMERIC',
+                    'value' => $today,
+                    'compare' => '>=', // Greater than or equal to value
+                    )
+                ),
+            );
+
+              ?>
               <?php $loop = new WP_Query( $args ); ?>
               <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
               <div class="event row align-center">
@@ -61,26 +45,24 @@
                   </div>
                 </div>
                 <div class="small-11 large-7 large-offset-1 columns event-copy">
+                    <?php if (get_field('link')) : ?>
                     <a href="<?php the_field('link'); ?>">
+                    <?php endif; ?>
                         <p class="event-date">
-                            <?php the_field('start_date'); ?>
-                        at
-                            <?php the_field('start_time'); ?>
+                            <?php the_field('start_date'); ?> at <?php the_field('start_time'); ?>
                         </p>
                         <h5 class="event-title">
                             <?php the_title();?>
                         </h5>
-                        <p class="event-locale">
-                            <?php the_field('location');?>
-                        </p>
-                        <p class="event-address">
-                        <?php the_field('address');?>
-                        </p>
+                        <p class="event-locale"><?php the_field('location');?></p>
+                        <span class="event-address"><?php the_field('address');?></span>
+                    <?php if (get_field('link')) : ?>
                     </a>
+                    <?php endif; ?>
                 </div>
             </div>
-             <?php endwhile; ?>
-          </div>
+          <?php endwhile; ?>
+         </div>
         </div>
     </div>
 </section>
