960f3953f0b7b99e7ef7eb0c0145a63cac62e028
Mica A <micada@users.noreply.github.com>
2017-03-04 09:39:28 -0600

    Revert "Mu-Plugins and Events CPT"

---
+++ wp-content/themes/pawar2018/page-events.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+  <main>
+  	<section class="hero inner-page" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/events-hero.jpg'); background-size: cover;">
+        <div class="row align-center align-middle">
+            <h1 class="page-title">Events</h1>
+       </div>
+  	</section>
+    <section class="body">
+        <div class="row align-center">
+            <div class="small-11 medium-5 large-4 small-order-2 medium-order-1 columns">
+                <h1 class="section-title">Let's Talk</h1>
+                <p>Looking to host an event?</p>
+                <a href='/get-involved' class="button">Contact Us</a>
+            </div>
+            <div class="small-12 medium-5 large-6 small-order-1 medium-order-2 columns">
+                <div class="event row align-center">
+                    <div class="small-3 medium-10 large-2 columns">
+                    <div class="event-date_circle">
+                        4<br>March
+                    </div>
+                    </div>
+                    <div class="small-11 large-7  large-offset-1 columns event-copy">
+                      <p class="event-date">Saturday, March 4th at 1:45PM</p>
+                      <h5 class="event-header">Brady Campaign</h5>
+                      <p class="event-locale">Simeon Career Academy</p>
+                      <p class="event-address">
+                        8147 S Vicennes Ave
+                        <br />
+                        Chicago, IL
+                      </p>
+                  </div>
+              </div>
+              <div class="event row align-center">
+                <div class="small-3 medium-10 large-2 columns">
+                    <div class="event-date_circle">
+                        5<br>March
+                    </div>
+                </div>
+                <div class="small-11 large-7 large-offset-1 columns event-copy">
+                    <p class="event-date">Sunday, March 5th at 4:30PM</p>
+                  <h5 class="event-header">Meet & Greet with Action for a Better Tomorrow - South Suburbs</h5>
+                  <p class="event-locale">Flossmoor Community Church</p>
+                  <p class="event-address">
+                    2218 Hutchinson Rd.
+                    <br />
+                    Flossmoor, IL
+                  </p>
+                </div>
+              </div>
+          </div>
+        </div>
+    </div>
+</section>
+</main>
+<?php get_footer(); ?>
