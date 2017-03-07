c700e5ed73a5b2580b59b14fdf8b6d017307d821
Peter <peter.binkowski@gmail.com>
2017-03-07 15:27:12 -0600

    just take out php loops from single pages

--- wp-content/themes/pawar2018/single.php
+++ wp-content/themes/pawar2018/single.php
@@ -2,13 +2,6 @@
 <main>
 	<section>
 		<div class="row">
-			<?php
-				if ( have_posts() ) {
-					while ( have_posts() ) {
-						the_post();
-					}
-				}
-			?>
 		</div>
 	</section>
 </main><!-- #main -->
