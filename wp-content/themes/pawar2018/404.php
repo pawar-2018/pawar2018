0fefd7d36207e854dff65174561b689ae857359c
Peter <peter.binkowski@gmail.com>
2017-03-06 22:16:17 -0600

    basic 404 page up

--- wp-content/themes/pawar2018/404.php
+++ wp-content/themes/pawar2018/404.php
@@ -1,64 +1,22 @@
-<?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package _s
- */
-
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
+<?php get_header(); ?>
+<main>
+	<section class="hero inner-page">
+		<div class="row align-center align-middle">
+            <h1 class="page-title">404 &mdash; Page Not Found</h1>
+       </div>
+	</section>
+	<section class="404 body">
+		<div class="row align-center">
+            <div class="small-11 medium-10 columns">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'Pawar2018' ); ?></h1>
 				</header><!-- .page-header -->
-
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'Pawar2018' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( _pawar2018_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'Pawar2018' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'Pawar2018' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+					<p><?php esc_html_e( 'It looks like we couldn&rsquo;t find what you were looking for. Sorry about that.', 'Pawar2018' ); ?></p>
 				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+			</div>
+		</div>
+	</section><!-- .error-404 -->
+</main><!-- #main -->
 <?php
 get_footer();
