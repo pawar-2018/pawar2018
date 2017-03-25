525be7d7774740a03309d1ef50cfa0c6d66462a7
David Altenburg <david@inventables.com>
2017-03-24 21:35:47 -0500

    Added press releases page

---
+++ wp-content/themes/pawar2018/archive-press-releases.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+<main>
+  <section class="hero inner-page" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/events-hero.jpg'); background-size: cover;">
+    <div class="row align-center align-middle">
+      <h1 class="page-title">Press Releases</h1>
+    </div>
+  </section>
+  <section class="body">
+    <div class="row align-center">
+      <div class="press-release mall-11 medium-11 large-11 small-order-2 medium-order-1 columns">
+        <h1 class="press-release__title section-title">Post One</h1>
+        <h6 class="press-release__date">March 4, 2017</h6>
+        <p>Lorem Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.  Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.  Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.
+
+</p>
+      </div>
+    </div>
+    <div class="press-release row align-center">
+      <div class="small-11 medium-11 large-11 small-order-2 medium-order-1 columns">
+        <h1 class="press-release__title section-title">Post Two</h1>
+        <h6 class="press-release__date">March 1, 2017</h6>
+        <p>Lorem Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.  Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.  Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.
+
+        </p>
+      </div>
+    </div>
+    <div class="press-release row align-center">
+      <div class="small-11 medium-11 large-11 small-order-2 medium-order-1 columns">
+        <h1 class="press-release__title section-title">Post Three</h1>
+        <h6 class="press-release__date">February 27, 2017</h6>
+        <p>Lorem Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.  Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.  Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.
+
+        </p>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer(); ?>
