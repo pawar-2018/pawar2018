a8ba21e96604c32cb00e07b4b2d34e5da73f3e7c
David Altenburg <david@inventables.com>
2017-04-06 22:50:00 -0500

    added press releases hero image

--- wp-content/themes/pawar2018/archive-press-releases.php
+++ wp-content/themes/pawar2018/archive-press-releases.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <main>
-  <section class="hero inner-page" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/events-hero.jpg'); background-size: cover;">
+  <section class="hero inner-page" style="background-image: url('<?php echo get_bloginfo('template_url') ?>/assets/press-releases-hero.jpg'); background-size: cover;">
     <div class="row align-center align-middle">
       <h1 class="page-title">Press Releases</h1>
     </div>
