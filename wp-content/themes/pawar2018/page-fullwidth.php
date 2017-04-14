91cc14c762dd0c800812bd7ea845f4d8b76508ae
Mica Alaniz <malaniz@leapfrogonline.com>
2017-04-13 19:56:59 -0500

    making the button fields

--- wp-content/themes/pawar2018/page-fullwidth.php
+++ wp-content/themes/pawar2018/page-fullwidth.php
@@ -13,10 +13,24 @@ get_header(); ?>
   <section class="wrapper">
     <div class="main body">
       <div class="row align-center">
-        <div class="small-12 medium-12 columns">
+        <div class="small-12 columns">
           <h1 class="section-title"><?php echo the_field('inner_section_title'); ?></h1>
           <?php the_content(); ?>
         </div>
+      </div>
+        <div class="row align-center">
+          <?php
+
+          $bottom_button = get_field('bottom_button');
+
+          if ($bottom_button !== 'no button') {
+              echo '<a href="' . $button_1_link . '" class="button">' . $button_1_text . '</a>';
+          }
+          if ($bottom_button == 'two buttons') {
+              echo '<a href="' . $button_2_link . '" class="button">' . $button_2_text . '</a>';
+          }
+
+          <?php endif; ?>
       </div>
     </div>
   </section>
