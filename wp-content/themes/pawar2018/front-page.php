d8c64ce486779cd02f37becc351b233ce76b0f58
Mica Alaniz <malaniz@leapfrogonline.com>
2017-04-17 19:44:07 -0500

    setting up the frontpage

--- wp-content/themes/pawar2018/front-page.php
+++ wp-content/themes/pawar2018/front-page.php
@@ -1,92 +1,80 @@
 <?php get_header(); ?>
   <main>
-    <section class="hero">
+    <section class="hero" style="background-image: url('<?php the_field('hero_image'); ?>');">
       <div class="row align-bottom">
         <div class="small-11 large-9 columns">
-          <h1>A New Deal For Illinois</h1>
+          <h1><?php the_field('hero_headline'); ?></h1>
         </div>
       </div>
       <div class="row align-middle">
         <div class="small-11 large-7 columns">
-          <h2>It‘s time to grab a clipboard, lace up our boots, and work together to restore progressive values in 2018.</h2>
+          <h2><?php the_field('hero_subheadline'); ?></h2>
         </div>
       </div>
       <div class="row align-middle">
         <div class="small-11 columns">
-          <h3>Get the latest updates</h3>
-          <a href="/newsletter" class="button">Subscribe</a>
+          <h3><?php the_field('hero_cta'); ?></h3>
+          <a href="<?php the_field('hero_button_link'); ?>" class="button">
+            <?php the_field('hero_button_text'); ?>
+          </a>
         </div>
       </div>
     </section>
     <section class="row main-content align-center align-middle">
+      <?php if (get_field('has_video')) : ?>
         <div class="small-11 large-6 columns">
           <div class="flex-video widescreen">
-            <iframe width="500" height="282" src="https://www.youtube.com/embed/0QN_PvW9nQw?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+            <?php the_field('featured_video'); ?>
           </div>
         </div>
       <div class="small-11 large-6 columns">
-        <h1 class="main-callout">I’m running for governor to forge a New Deal for Illinois.</h1>
+      <?php else: ?>
+      <div class="small-11 large-4 columns">
+        <h1 class="main-callout"><?php the_field('main_callout'); ?></h1>
+      </div>
+      <div class="small-11 large-8 columns">
+      <?php endif; ?>
+        <?php get_field('has_video') ? echo '' : echo '<h1 class="main-callout">' . get_field('main-callout') . '</h1>';
+        ?>
         <p class="main-copy">
-          President Roosevelt’s New Deal took our country out of depression and laid the foundation for decades of growth and prosperity. We can do the same thing in Illinois by rebuilding our crumbling infrastructure, reinvesting in good-paying jobs, and recommitting to early childhood education and public schools. And we can do it without further burdening the middle class.
+          <?php the_field('callout_copy'); ?>
         </p>
       </div>
     </section>
     <section class="action-content">
       <div class="pillar-wrapper">
-        <h4 class="section-title">Ameya's Four Pillars</h4>
+        <h4 class="section-title"><?php the_field('pillar_headline'); ?></h4>
         <h1 class="main-callout">
-          What's in a New Deal for 2018.
+          <?php the_field('pillar_subheadline'); ?>
         </h1>
         <div class="row small-up-1">
-          <div class="column column-block pillar-content">
-            <img src="<?php echo get_bloginfo('template_url') ?>/assets/school.svg" class="pillar-icon" alt="School Icon">
-            <div class="pillar-copy">
-              <h4 class="pillar-header">Education</h4>
-              <p>
-                Increase funding to all public schools by eliminating corporate tax loopholes and make millionaires pay their fair share.
-              </p>
+          <?php $loop = new WP_Query( $args ); ?>
+          <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+            <div class="column column-block pillar-content">
+              <img src="<?php echo get_field('pillar_icon') ?>" class="issue-icon" alt="<?php echo get_field('pillar_alt') ?>">
+              <div class="pillar-copy">
+                <h4 class="pillar-header"><?php the_title();?></h4>
+                <p><?php the_content() ?></p>
+              </div>
             </div>
-          </div>
-          <div class="column column-block pillar-content">
-            <img src="<?php echo get_bloginfo('template_url') ?>/assets/family.svg" class="pillar-icon" alt="Family Icon">
-            <div class="pillar-copy">
-              <h4 class="pillar-header">Childcare</h4>
-              <p>
-                Provide universal access to childcare and support working families with paid sick leave, fair scheduling practices, parental leave, and a living wage.
-              </p>
-            </div>
-          </div>
-          <div class="column column-block pillar-content">
-            <img src="<?php echo get_bloginfo('template_url') ?>/assets/bridge.svg" class="pillar-icon" alt="Bridge Icon">
-            <div class="pillar-copy">
-              <h4 class="pillar-header">Jobs & Infrastructure</h4>
-              <p>
-                Create tens of thousands of new middle-class jobs with a New Deal infrastructure program.
-              </p>
-            </div>
-          </div>
-          <div class="column column-block pillar-content">
-            <img src="<?php echo get_bloginfo('template_url') ?>/assets/hammer.svg" class="pillar-icon" alt="Gavel Icon">
-            <div class="pillar-copy">
-              <h4 class="pillar-header">criminal justice reform</h4>
-              <p>
-                Pass criminal justice reform and refocus resources from prisons to diversion programs, job training and placement, and social and mental health services.
-              </p>
-            </div>
-          </div>
+            <?php
+            // clean up after the query and pagination
+            wp_reset_postdata();
+            ?>
+          <?php endwhile; ?>
         </div>
         <div class="pillar-button">
-          <a href="<?php echo esc_url( home_url( '/issues' )) ?>" class="button">
-            See the Issues
+          <a href="<?php the_field('pillar_button_link'); ?>" class="button">
+            <?php the_field('pillar_button_text'); ?>
           </a>
         </div>
       </div>
       <div class="event-wrapper">
         <div class="event-content">
           <h4 class="section-title">
-            Upcoming Events
+            <?php the_field('event_headline'); ?>
           </h4>
-          <h1 class="main-callout">Let's talk.</h1>
+          <h1 class="main-callout"><?php the_field('event_subheadline'); ?></h1>
           <div class="row small-collapse">
              <?php
                 $today = date('Ymd');
@@ -156,26 +144,26 @@
           </div>
         </div>
         <div class="event-photo">
-          <img src="<?php echo get_bloginfo('template_url') ?>/assets/ameya02.jpg" class="event-photo" alt="Ameya Pawar with Microphone">
+          <img src="<?php echo get_field('event_photo') ?>" class="event-photo" alt="<?php echo get_field('event_photo_alt') ?>">
         </div>
       </div>
     </section>
     <div class="row align-middle align-center">
       <div class="small-11 large-10 columns">
         <h1 class="main-quote">
-          "We have the power to use government as a force for good—if we elect people who believe in the power of government to improve the lives of people in every community across Illinois. That’s why I’m running for Governor.”
+          <?php the_field('main_quote'); ?>
         </h1>
         <p class="main-caption">
-          - Ameya Pawar
+          <?php the_field('quote_caption'); ?>
         </p>
       </div>
     </div>
     <div class="row bottom-content small-collapse align-middle align-justify">
       <div class="small-12 large-6 columns">
-        <img src="<?php echo get_bloginfo('template_url') ?>/assets/ameya03.jpg" alt="Ameya Pawar with Schoolchildren">
+        <img src="<?php echo get_field('bottom_first_photo') ?>" alt="<?php echo get_field('bottom_first_photo_alt') ?>">
       </div>
       <div class="small-12 large-6 columns">
-        <img src="<?php echo get_bloginfo('template_url') ?>/assets/ameya04.jpg" alt="Ameya Pawar in front of a construction site">
+        <img src="<?php echo get_field('bottom_second_photo') ?>" alt="?php echo get_field('bottom_second_photo_alt') ?>">
       </div>
     </div>
   </div>
