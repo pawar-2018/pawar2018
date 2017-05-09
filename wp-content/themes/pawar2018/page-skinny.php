feb94252aae95a2fad939f5c966531c0429af660
Stephen Lund <lund5000@gmail.com>
2017-05-09 00:00:26 -0500

    Use custom field for form ID

--- wp-content/themes/pawar2018/page-skinny.php
+++ wp-content/themes/pawar2018/page-skinny.php
@@ -10,7 +10,7 @@ get_header('skinny'); ?>
       <div class="row align-center">
         <div class="small-12 medium-12 columns">
           <?php the_content(); ?>
-          <div class="ngp-form" data-id="9188561423484586496"></div>
+          <div class="ngp-form" data-id="<?php echo get_post_meta(get_the_ID(), 'form_id', true); ?>"></div>
           <p></p>
         </div>
       </div>
