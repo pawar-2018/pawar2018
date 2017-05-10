bf2ef7ed413876664f52439e8a0077f0c5710978
Brian Montana <Brian.Montana@blastradius.com>
2017-05-09 20:57:09 -0500

    Media Signup and Media Packet on Press Release template.

--- wp-content/themes/pawar2018/archive-press-releases.php
+++ wp-content/themes/pawar2018/archive-press-releases.php
@@ -42,9 +42,17 @@
     <aside class="sidebar" role="complementary">
       <div class="row align-center">
         <div class="small-12 medium-8 columns">
-          <p><strong>Contact: Tom Elliott</strong><br/>
-          773-819-0503<br/>
-          <a href="mailto:media@pawar2018.com">media@pawar2018.com</a></p>
+          <p>
+          <strong>Contact: Tom Elliott</strong>
+          <br>
+          773-819-0503
+          <br>
+          <a href="mailto:media@pawar2018.com">media@pawar2018.com</a>
+          <br>
+          <a href="//www.pawar2018.com/wp-content/uploads/2017/05/Pawar-for-Governor-Media-Packet.pdf" target="_blank">Media Packet</a>
+          <br>
+          <a href="//goo.gl/forms/lktSWFnm0NvyqUR82" target="_blank">Media Signup</a>
+          </p>
         </div>
       </div>
     </aside>
