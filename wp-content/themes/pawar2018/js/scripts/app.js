4836821755c0385d9d42420e90d491d3ab764789
Stephen Lund <lund5000@gmail.com>
2017-05-13 20:30:44 -0500

    Use Foundation .is-active class for plus/minus. Update accordion settings and remove some redundant custom style declarations. Add -light-gray color variable.

--- wp-content/themes/pawar2018/js/scripts/app.js
+++ wp-content/themes/pawar2018/js/scripts/app.js
@@ -19,21 +19,18 @@
   var hash = window.location.hash.substring(1);
 
   for (i = 0; i < accords.length; ++i) {
-    let accordContent = accords[i].querySelector(".accordion-content");
-    let accordTitle = accords[i].querySelector(".accordion-title");
+    let item = accords[i];
+    let accordTitle = item.querySelector('.accordion-title');
     if (window.location.hash) {
       if (hash === accordTitle.getAttribute('name')) {
-        accordContent.classList.add('accordion-content--active');
-        accordTitle.classList.add('accordion-title--active');
+        item.classList.add('is-active');
       }
     }
     accordTitle.addEventListener('click', function(event) {
-      if(!accordContent.classList.contains('accordion-content--active')) {
-        accordContent.classList.add('accordion-content--active');
-        accordTitle.classList.add('accordion-title--active');
+      if(!item.classList.contains('is-active')) {
+        item.classList.add('is-active');
       } else {
-        accordContent.classList.remove('accordion-content--active');
-        accordTitle.classList.remove('accordion-title--active');
+        item.classList.remove('is-active');
       }
     })
   }
