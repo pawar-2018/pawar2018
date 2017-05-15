d7c1b120d604999ead7176dbf5aed4cdff9faf37
Peter <peter.binkowski@gmail.com>
2017-05-14 23:14:19 -0500

    change header

--- wp-content/themes/pawar2018/home.php
+++ wp-content/themes/pawar2018/home.php
@@ -2,13 +2,7 @@
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <header class="page-header ">
-                <section class="hero inner-page" style="background-image: url('<?php the_field('hero_image'); ?>');">
-                  <div class="row align-center align-middle">
-                    <h1 class="page-title"><?php the_title(); ?></h1>
-                  </div>
-                </section>
-            </header><!-- .page-header -->
+            <h1 class="page-title"><?php the_title(); ?></h1>
 			<div class="row align-center">
 				<?php if ( have_posts() ) : ?>
 					<?php while ( have_posts() ) : the_post();
