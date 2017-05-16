01a6cdbf229f6161f9b89baaeffd44e41b875656
Peter <peter.binkowski@gmail.com>
2017-05-16 14:35:14 -0500

    single post work

--- wp-content/themes/pawar2018/single.php
+++ wp-content/themes/pawar2018/single.php
@@ -1,14 +1,17 @@
 <?php get_header(); ?>
 <main>
-	<section>
-		<div class="row align-center">
-			<div class="small-8 columns">
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-					get_template_part( 'template-parts/content-post', get_post_format() );
+	<section class="wrapper">
+		<div class="main body">
+			<div class="row align-center">
+				<div class="small-10 columns">
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+						get_template_part( 'template-parts/content-post', get_post_format() );
 
-				endwhile; ?>
+					endwhile; ?>
+					<a href="/new-deal-journal">&larr; Back to the journal</a>
+				</div>
 			</div>
 		</div>
 	</section>
