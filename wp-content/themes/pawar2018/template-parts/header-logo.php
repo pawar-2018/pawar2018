5c04878c5c10a2b71a5fec0e7af9612b114770a4
Mica Alaniz <malaniz@leapfrogonline.com>
2017-06-04 21:29:30 -0500

    adding this new plugin

--- wp-content/themes/pawar2018/template-parts/header-logo.php
+++ wp-content/themes/pawar2018/template-parts/header-logo.php
@@ -1,3 +1,10 @@
 <a class="header-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" aria-label="Back to the Home Page">
-	<img src="<?php echo get_bloginfo('template_url') ?>/assets/logo.svg" alt="Ameya Pawar 2018 Logo">
+  <?php
+    $header_menu_items = get_items_by_location('header-menu');
+    foreach ($header_menu_items as $item) {
+      if ($item->title === "Meet Ameya") {
+        echo "<img src=" . get_field( 'main_logo', $item->ID ) . " alt='Ameya Pawar 2018 Logo'/>";
+      }
+    }
+  ?>
 </a>
