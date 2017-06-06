14d1abbd2658a3303b0e14ac7cb4c571a8074332
Mica Alaniz <malaniz@leapfrogonline.com>
2017-06-06 13:20:37 -0500

    fixing markup errors

--- wp-content/themes/pawar2018/js/scripts/app.js
+++ wp-content/themes/pawar2018/js/scripts/app.js
@@ -35,5 +35,16 @@
     })
   }
 
+  function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i=0; i<vidDefer.length; i++) {
+      if(vidDefer[i].getAttribute('data-src')) {
+        vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+      }
+    }
+  }
+
+  window.onload = init;
+
 })();
 
