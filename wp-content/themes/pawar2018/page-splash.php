86c972574ce824d629b2d3b935638848af6727d5
Mica Alaniz <malaniz@leapfrogonline.com>
2017-06-14 00:03:20 -0500

    splash page needed for vth

--- wp-content/themes/pawar2018/page-splash.php
+++ wp-content/themes/pawar2018/page-splash.php
@@ -3,15 +3,18 @@
 Template name: Splash Template
  */
 ?>
+<?php get_template_part('template-parts/header', 'head'); ?>
 
 <main>
   <section class="wrapper">
     <div class="splash main" style="background-image: url('<?php the_field('splash_image'); ?>');">
-      <div class="row align-center align-top content">
-        <div class="small-10 medium-5 columns">
-          <img src="<?php echo get_bloginfo('template_url') ?>/assets/pawar2018_logo.png" alt="Ameya Pawar 2018 Logo">
+      <div class="row align-center align-middle content">
+        <div class="small-10 medium-4 columns">
+        <a href="<?php the_field('splash_link'); ?>" target="_blank">
+          <img src="<?php the_field('splash_logo'); ?>" alt="Ameya Pawar 2018 Logo">
+        </a>
         </div>
-        <div class="small-10 medium-5 columns">
+        <div class="small-10 medium-6 columns">
           <?php the_content(); ?>
         </div>
       </div>
