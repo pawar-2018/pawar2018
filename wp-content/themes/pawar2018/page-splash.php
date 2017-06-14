b1af8fab2e6814b5999c54be0b890865e0b28551
Mica Alaniz <malaniz@leapfrogonline.com>
2017-06-14 12:08:02 -0500

    adding social buttons

--- wp-content/themes/pawar2018/page-splash.php
+++ wp-content/themes/pawar2018/page-splash.php
@@ -18,6 +18,27 @@ Template name: Splash Template
           <?php the_content(); ?>
         </div>
       </div>
+        <div class="row align-middle social">
+          <div class="small-10 large-6 columns">
+            <div class="splash-social">
+              <a class="splash-social__link" href="https://www.facebook.com/AmeyaPawarIL/" target="_blank" aria-label="Link to Facebook AmeyaPawarIL">
+                <img src="<?php echo get_bloginfo('template_url') ?>/assets/facebook.svg" alt="Facebook Icon">
+              </a>
+              <a class="splash-social__link" href="https://twitter.com/Ameya_Pawar_IL" target="_blank" aria-label="Link to Twitter Ameya_Pawar_IL">
+                <img src="<?php echo get_bloginfo('template_url') ?>/assets/twitter.svg" alt="Twitter Icon">
+              </a>
+              <a class="splash-social__link" href="https://www.instagram.com/ameya_pawar_IL" target="_blank" aria-label="Link to Instagram ameya.s.pawar">
+                <img src="<?php echo get_bloginfo('template_url') ?>/assets/instagram.svg" alt="Instagram Icon">
+              </a>
+              <a class="splash-social__link" href="https://www.youtube.com/user/RenewChicago/videos" target="_blank" aria-label="Link to Youtube RenewChicago">
+                <img src="<?php echo get_bloginfo('template_url') ?>/assets/youtube.svg" alt="Youtube Icon">
+              </a>
+              <a class="splash-social__link" href="https://shop.pawar2018.com" target="_blank" aria-label="Link to Campaign Shop">
+                <img src="<?php echo get_bloginfo('template_url') ?>/assets/shopping-icon2.svg" alt="Shop Icon">
+              </a>
+            </div>
+          </div>
+        </div>
     </div>
   </section>
 </main>
