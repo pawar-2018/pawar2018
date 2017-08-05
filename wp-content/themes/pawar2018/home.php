b3128dd53419ea3849001d3e9d19cd7db2e60682
Peter <peter.binkowski@gmail.com>
2017-08-05 11:49:22 -0400

    tags and categories

--- wp-content/themes/pawar2018/home.php
+++ wp-content/themes/pawar2018/home.php
@@ -21,6 +21,7 @@
                                     <?php the_post_thumbnail(); ?>
                                 </aside>
                                 <div class="article-inner">
+                                    <?php the_category(); ?>
                                     <header class="entry-header">
                                         <?php
                                         if ( is_single() ) :
@@ -30,15 +31,16 @@
                                         endif;
 
                                         if ( 'post' === get_post_type() ) : ?>
-                                        <h6 class="entre-date">
-                                          <?php the_date('F j, Y'); ?>
-                                        </h6>
+
                                         <?php
                                         endif; ?>
                                     </header><!-- .entry-header -->
                                     <div class="entry-content">
                                         <?php the_excerpt(); ?>
                                     </div><!-- .entry-content -->
+                                    <div class="tags">
+                                        <?php the_tags($before = '', $sep = ' ', $after = ''); ?>
+                                    </div>
                                 </div>
                             </article><!-- #post-## -->
         				<?php endwhile; ?>
