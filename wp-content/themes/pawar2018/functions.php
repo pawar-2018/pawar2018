de75cbe8359e01d824a68ccfd401924a9e455324
Peter <peter.binkowski@gmail.com>
2017-08-05 17:59:28 -0400

    start of the social buttons.

--- wp-content/themes/pawar2018/functions.php
+++ wp-content/themes/pawar2018/functions.php
@@ -236,3 +236,8 @@ function wpdocs_custom_excerpt_length( $length ) {
     return 20;
 }
 add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+function custom_excerpt_more( $more ) {
+	return '...';
+}
+add_filter( 'excerpt_more', 'custom_excerpt_more' );
--- wp-content/themes/pawar2018/single.php
+++ wp-content/themes/pawar2018/single.php
@@ -4,12 +4,13 @@
 		<div class="main body">
 			<div class="row align-center">
 				<div class="small-12 medium-10 columns">
-					<?php
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
-						get_template_part( 'template-parts/content-post', get_post_format() );
+					<?php while ( have_posts() ) : the_post(); ?>
+						<?php get_template_part( 'template-parts/content-post', get_post_format());?>
 
-					endwhile; ?>
+						<a class="social-btn twitter" target="_blank" href="https://twitter.com/share?url=<?php echo(the_permalink());?>&via=ameya_pawar_il&text=<?php echo(the_title()); ?>">
+						<i class="fa fa-twitter"></i>Tweet
+					   </a>
+					<?php endwhile; ?>
 
 				</div>
 			</div>
