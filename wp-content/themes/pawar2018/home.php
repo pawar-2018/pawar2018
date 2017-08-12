ae12c623ed6502a4d50dc639def15accc0723043
Peter <peter.binkowski@gmail.com>
2017-08-12 17:53:29 -0500

    drop down styling

--- wp-content/themes/pawar2018/home.php
+++ wp-content/themes/pawar2018/home.php
@@ -13,7 +13,7 @@
                   <?php
                     $tags = get_tags();
                     $html = '<div class="post_tags">Sort By <select onChange="window.location.href=this.value">';
-                    $html .= "<option>Issue</option>";
+                    $html .= "<option>All Issues</option>";
                     foreach ( $tags as $tag ) {
                       $tag_link = get_tag_link( $tag->term_id );
                       $html .= "<option value='{$tag_link}'><a href='{$tag_link}' title='{$tag->name} Tag' class='{$tag->slug}'>";
