8eb6bf5fb06a0db25b92727bd43e129a36d79035
Peter <peter.binkowski@gmail.com>
2017-08-12 18:26:22 -0500

    pagination

--- wp-content/themes/pawar2018/home.php
+++ wp-content/themes/pawar2018/home.php
@@ -60,6 +60,21 @@
                             </article><!-- #post-## -->
         				<?php endwhile; ?>
         				<?php endif; ?>
+                        <div class="paginate_links small-12 columns">
+                            <?php
+                            global $wp_query;
+
+                            $big = 999999999; // need an unlikely integer
+
+                            echo paginate_links( array(
+                            	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                            	'format' => '?paged=%#%',
+                            	'current' => max( 1, get_query_var('paged') ),
+                            	'total' => $wp_query->max_num_pages,
+                                'type' => 'list'
+                            ) );
+                            ?>
+                        </div>
                     </div>
 			   </div>
             </div>
